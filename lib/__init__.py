e619b0b6a9fcb36685c8b9b9a56a014a2fb42e9e
Your GitHub Username <Your GitHub Email>
2025-09-18 14:42:08 +0530

    Include MONAI library in repository - remove contributor exclusions

---
+++ lib/__init__.py
@@ -0,0 +1,9 @@
+# Medical Image Classification Package
+# Contains MONAI framework and related utilities
+
+# Version info
+__version__ = "1.0.0"
+__author__ = "Medical AI Team"
+
+# Note: MONAI modules are available but not auto-imported
+# Import them explicitly when needed: from lib.monai import *
